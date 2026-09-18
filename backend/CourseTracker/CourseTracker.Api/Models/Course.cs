3ce26338b7e95c6b3310c8617a81670647589d1d
Zakharko Adrian <zakharkoadr@gmail.com>
2026-09-18 17:52:30 +0300

    Created Models

---
+++ backend/CourseTracker/CourseTracker.Api/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿namespace CourseTracker.Api.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Lesson> Lessons { get; set; } = new List<Lesson>();
+    }
+}
---
+++ backend/CourseTracker/CourseTracker.Api/Models/Lesson.cs
@@ -0,0 +1,15 @@
+﻿namespace CourseTracker.Api.Models
+{
+    public class Lesson
+    {
+        public int Id { get; set; }
+        public string CourseId { get; set; } = String.Empty;
+        public string Title { get; set; } = String.Empty;
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public Course? Course { get; set; }
+
+    }
+}
